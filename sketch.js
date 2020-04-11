6e7153397341c8a6a69bdbfdab53fec53f8df2e5
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-12 04:23:46 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,13 @@
+const cwidth[]=[400,1150,450,500,1500,1100,1500,800,1550,1150,750];
+const cheight[]=[450,1000,1200,750,1100,250,300,900,650,600,300];
+
+const diameter = 50;
+
+const rSize[] =[500,500,500,500,500,500,500,500,500,500,500];
+const rSize0[] =[500,500,500,500,500,500,500,500,500,500,500];
+
+
+
 function setup(){
   createCanvas(windowWidth, windowHeight);//描画領域を指定
   background(255);
