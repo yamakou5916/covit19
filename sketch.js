c33f8f27ac95c8fbb6f8e860e7eb37147abc6236
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-12 05:22:24 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,12 +6,17 @@ const rSize0 =[500,500,500,500,500,500,500,500,500,500,500];
 const lSize = [0];
 const collision = 0;
 const bigText = ["能力と社会性の変化","生活と価値観の変化","余暇と気晴らしの変化","職業と人生観の変化","社会通念と手続きの変化","個人情報とプライバシーの変化","人・社会との繋がりの変化","ものづくりの変化","所属意識の変化","責任領域の変化","信頼形成の変化"];
-
-const images = [loadImage("education.png"),loadImage("lifestyle.png"),loadImage("entertainment.png"),loadImage("workstyle.png"),loadImage("lifeevent.png"),loadImage("healthcare.png"),loadImage("lifeline.png"),loadImage("manufacture.png"),loadImage("supplychain.png"),loadImage("management.png"),loadImage("medical.png")];
+var pathList = ["education.png","lifestyle.png","entertainment.png","workstyle.png","lifeevent.png","healthcare.png","lifeline.png","manufacture.png","supplychain.png","management.png","medical.png"];
 
 function setup(){
   createCanvas(windowWidth, windowHeight);//描画領域を指定
   background(255);
+  var img = new Array();
+  for (var i = 0; i < pathList.length; i++) {
+      var image = new Image();
+      image.src = pathList[i];
+      img.push(image);
+  }
 }
 
 function draw(){
@@ -32,7 +37,6 @@ function draw(){
   text("集団としての意識", width-100, height/2);
   text("文化に関する思想", width/2, height-40);
   text("社会システムに関する思想", width/2, 0+40);
-  text(bigText, width/2,height/2);
 
   for(var i=0; i<11;i++){
     //text("社会システムに関する思想", width/2,height/2);
@@ -41,9 +45,10 @@ function draw(){
     fill(0);
     noStroke();
     imageMode(CENTER);
-    image(images[i], width/2,height/2, 70, 70);
+    image(img[i], width/2,height/2, 70, 70);
     textSize(12);
     textAlign(CENTER,CENTER);
+    text(bigText[i], cwidth[i], cheight[i]+25;
     textSize(10);
     textAlign(CENTER,CENTER);
     //text(titleText[i][collision], cwidth[i], cheight[i]-45);
