1ce4c16c91b309f199a39ed5b41ae9a3023c0584
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-12 22:41:11 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -28,34 +28,6 @@ const bCol=[191,191,144,144,125,125,200,200,186,186,187,187];
 let images0;
 let allImages = [];
 
-
-// center point
-var centerX = 0.0, centerY = 0.0;
-
-var radius = 45, rotAngle = -90;
-var accelX = 0.0, accelY = 0.0;
-var deltaX = 0.0, deltaY = 0.0;
-var springing = 0.0009, damping = 0.98;
-
-//corner nodes
-var nodes = 5;
-
-//zero fill arrays
-var nodeStartX = [];
-var nodeStartY = [];
-var nodeX = [];
-var nodeY = [];
-var angle = [];
-var frequency = [];
-
-// soft-body dynamics
-var organicConstant = 1.0;
-
-
-
-
-
-
 function setup(){
   createCanvas(2000, 1500);//描画領域を指定
   background(255);
@@ -72,30 +44,6 @@ function setup(){
   allImages.push(loadImage("supplychain.png"));
   allImages.push(loadImage("management.png"));
   allImages.push(loadImage("medical.png"));
-
-
-  //center shape in window
-  centerX = width/2;
-  centerY = height/2;
-
-  //initialize arrays to 0
-  for (var i=0; i<nodes; i++){
-    nodeStartX[i] = 0;
-    nodeStartY[i] = 0;
-    nodeY[i] = 0;
-    nodeY[i] = 0;
-    angle[i] = 0;
-  }
-
-  // iniitalize frequencies for corner nodes
-  for (var i=0; i<nodes; i++){
-    frequency[i] = random(5, 12);
-  }
-
-  noStroke();
-  frameRate(30);
-
-
 
 }
 
@@ -134,10 +82,6 @@ function draw(){
     textAlign(CENTER,CENTER);
     text(titleText[i], cwidth[i], cheight[i]-45);
   }
-
-  drawShape();
-  moveShape();
-
 
 }
 
@@ -219,57 +163,6 @@ function littleCircle(i){
   }
 
 
-function drawShape() {
-  stroke(rCol[i],gCol[i],bCol[i]);
-  //  calculate node  starting locations
-  for (var i=0; i<nodes; i++){
-    nodeStartX[i] = centerX+cos(radians(rotAngle))*radius;
-    nodeStartY[i] = centerY+sin(radians(rotAngle))*radius;
-    rotAngle += 360.0/nodes;
-  }
-
-  // draw polygon
-  curveTightness(organicConstant);
-  fill(255);
-  beginShape();
-  for (var i=0; i<nodes; i++){
-    curveVertex(nodeX[i], nodeY[i]);
-  }
-  for (var i=0; i<nodes-1; i++){
-    curveVertex(nodeX[i], nodeY[i]);
-  }
-  endShape(CLOSE);
-}
-
-function moveShape() {
-  //move center point
-  deltaX = mouseX-centerX;
-  deltaY = mouseY-centerY;
-
-  // create springing effect
-  deltaX *= springing;
-  deltaY *= springing;
-  accelX += deltaX;
-  accelY += deltaY;
-
-  // move predator's center
-  centerX += accelX;
-  centerY += accelY;
-
-  // slow down springing
-  accelX *= damping;
-  accelY *= damping;
-
-  // change curve tightness
-  organicConstant = 1-((abs(accelX)+abs(accelY))*0.1);
-
-  //move nodes
-  for (var i=0; i<nodes; i++){
-    nodeX[i] = nodeStartX[i]+sin(radians(angle[i]))*(accelX*2);
-    nodeY[i] = nodeStartY[i]+sin(radians(angle[i]))*(accelY*2);
-    angle[i] += frequency[i];
-  }
-}
 
 
 
