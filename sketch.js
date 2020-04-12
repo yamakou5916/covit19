e42ef9fd03031152f62d954b55351e9e06fd3557
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-12 11:40:58 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -90,7 +90,7 @@ void myCircle(int i) {
     strokeWeight(2);
 }
   stroke(100,100,0); //点の色は青
-  for (int h = 0; h < numsX.length; h ++) {
+  for (int h = 0; h < numsX.length; h++) {
     var x = numsX[i][h];
     var y = numsY[i][h];
     point(x, y);
