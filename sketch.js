13bde7ba06f8e3e51750191e7c29828698f17f5a
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-13 00:56:45 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -90,8 +90,8 @@ function draw(){
       }
       peaks.push(createVector(sin(radians(i)) * j, cos(radians(i)) * j));
       peaks.forEach(peak => {
-        peak.x += (noise(xnoiseCords[i % 360 / 30]) * 120) * h;
-        peak.y += (noise(ynoiseCords[i % 360 / 30]) * 120) * h;
+        peak.x += (noise(xnoiseCords[i % 360 / 30]) * 120) * h +400;
+        peak.y += (noise(ynoiseCords[i % 360 / 30]) * 120) * h +400;
       });
       noFill();
       //fill(80, 180 + (180 / repnum * h), 100,80);
