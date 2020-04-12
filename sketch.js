d03f1661b13bc9f2b3409617f03193048efc0606
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-12 13:19:12 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -56,28 +56,28 @@ function draw(){
 
 
 function randomSave() {
-  var numsX = new Array(cwidth.length);
+  numsX = new Array(cwidth.length);
   for(let y = 0; y < cwidth.length; y++) {
     numsX = new Array(2000).fill(0);
   } 
-  var numsY = new Array(cwidth.length);
+  numsY = new Array(cwidth.length);
   for(let y = 0; y < cwidth.length; y++) {
     numsY = new Array(2000).fill(0);
   }
 
-    for(var i=0; i<cwidth.length;i++){
-      var radius = rSize0[i]/4;
-      var count = 1500;
-      for (var h = 0; h < count; h ++) {
-        var angle = random(TWO_PI);
-        var r = sqrt(random(1));
-        var x = cwidth[i]+ r * radius * cos(angle);
-        var y = cheight[i] + r * radius * sin(angle);
-        numsX[i][h] = x;
-        numsY[i][h] = y;
-      }
+  for(var i=0; i<cwidth.length;i++){
+    var radius = rSize0[i]/4;
+    var count = 1500;
+    for (var h = 0; h < count; h ++) {
+      var angle = random(TWO_PI);
+      var r = sqrt(random(1));
+      var x = cwidth[i]+ r * radius * cos(angle);
+      var y = cheight[i] + r * radius * sin(angle);
+      numsX[i][h] = x;
+      numsY[i][h] = y;
     }
-    text(numsX[i][h], width/2,height/2);
+  }
+  text(numsX[i][h], width/2,height/2);
 
 }
 
