d7f83879a4b1d7be40565b2f991f13eb6f11bb14
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-12 15:49:20 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -76,6 +76,16 @@ function randomSave() {
         numsX[i].push(x);
         numsY[i].push(y);
       }
+    }else{
+      for (int h = 0; h < count; h ++) {
+        var angle = random(TWO_PI);
+        var r = 1-(random(random(random(1))));
+        var x = cwidth[i]+ r * radius * cos(angle);
+        var y = cheight[i] + r * radius * sin(angle);
+        numsX[i].push(x);
+        numsY[i].push(y);
+      }
+
     }
   }
 
