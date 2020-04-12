3e972911196c571d07a0889ae5b179371b93a6ba
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-13 06:54:44 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -107,7 +107,7 @@ for (var t=0; t<twidth.length; t++){
         curveVertex(peak.x, peak.y);
       });
     }
-    text(peak[0], width/2, height/2);
+    text(peaks, width/2, height/2);
     endShape();
     noiseUpdate();
   }
