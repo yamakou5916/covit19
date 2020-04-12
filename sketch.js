6664a9ae92928ae8c8746cf1494f44ba6ee40092
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-12 15:02:10 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -89,7 +89,7 @@ function randomSave() {
 function myCircle(i){
   strokeWeight(3);
   stroke(100,100,0);
-  text(numsX[0][0], width/2,height/2);
+  //text(numsX[0][0], width/2,height/2);
   for (var h = 0; h < 10; h++){
     //var x = numsX[i][h];
     //var y = numsY[i][h];
