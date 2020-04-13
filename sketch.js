65a0556792ce9722da633e851e4ee6db17554857
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-13 12:23:06 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -83,6 +83,8 @@ function draw(){
   text("社会システムに関する思想", width/2, 0+40);
 
   //テクノロジマップ
+
+if(mousePressed){
 for (var t=0; t<twidth.length; t++){
   for (var h = 0; h < repnum; h++) {
     beginShape();
@@ -117,7 +119,7 @@ for (var t=0; t<twidth.length; t++){
     k = 0;
   }
 }
-
+}
 
   //意識・思想マップ
   for(var i=0; i<11;i++){
