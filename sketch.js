d59a7d93d8bb4d86f50137b96ea077c3ce76eef1
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-14 19:19:11 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -313,8 +313,8 @@ function showPwC(){
   
 function mouseClicked(){
   document.getElementById("text").innerHTML = "クリックされた！";
-  document.getElementById('popup_overlay').style.transform = scale(1);
-
+  //document.getElementById('popup_overlay').style.transform = scale(1);
+  $('.popup_overlay').show();
   //text(titleText[i], width/2, height/2);
 };
 
