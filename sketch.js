5fbd20e1ac454a27b13c6f40912a86f8c8f559e9
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-15 11:30:16 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -104,6 +104,7 @@ function draw(){
   //
   if(framCnt<120){
     moss();
+    wird();
   }
   
 
@@ -324,8 +325,22 @@ function moss() {
         //point(x, y);
     }
   }
-
 }
+
+function wird(){
+  var radius = rSize0[i]/16;
+    var count = 4000;
+    numsX.push([i]);
+    numsY.push([i]);
+    for (var h = 0; h < count; h ++) {
+      var angle = random(TWO_PI);
+      var r = sqrt(random(1));
+      var x = width/2+random(-1000,1000);
+      var y = height/2+random(-750,750);
+      numsX[i].push(x);
+      numsY[i].push(y);
+}
+
 
 
 function noiseSet() {
