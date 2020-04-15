86fe0cff09984ecabdee23b65c9547cd21c6fbc4
yamakou5916 <koki.yamamo10@gmail.com>
2020-04-15 11:41:24 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -310,7 +310,7 @@ function littleCircle(i){
 function moss() {
   for(var i=0; i<11;i++){
     var radius = rSize0[i]/16;
-    var count = 10;
+    var count = 40;
     numsX.push([i]);
     numsY.push([i]);
       for (var h = 0; h < count; h ++) {
@@ -330,7 +330,7 @@ function moss() {
 function wird(){
   for(var i=0; i<11;i++){
     var radius = rSize0[i];
-      var count = 40;
+      var count = 10;
       numsX.push([i]);
       numsY.push([i]);
       for (var h = 0; h < count; h ++) {
