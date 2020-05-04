8523ff5a0b9ee9b9ed7746f109ffdb1808a48b75
yamakou5916 <koki.yamamo10@gmail.com>
2020-05-04 16:31:49 +0900

    Add files via upload

--- timeseries.js
+++ timeseries.js
@@ -8,33 +8,4 @@ var chart = c3.generate({
     }
 });
 
-data: {
-     colors: {
-        data1: function(d) {
-            var color = '#000000';
-            switch (d.index){
-                case 0:
-                    color = 'rgb(255, 40, 0)';
-                    break;
-                case 1:
-                    color = 'rgb(0, 65, 255)';
-                    break;
-                case 2:
-                    color = 'rgb(250, 153, 0)';
-                    break;
-                case 3:
-                    color = 'rgb(53, 161, 107)';
-                    break;
-                case 4:
-                    color = 'rgb(255 ,153 ,160)';
-                    break;
-                case 5:
-                    color = 'rgb(102, 51, 0)';
-                    break;
-            } 
-            return  color;
-        }
-    }
-}
-
 
