c240a8560383c8276bdb462c8351f6d56c25007d
yamakou5916 <koki.yamamo10@gmail.com>
2020-05-06 14:50:10 +0900

    Add files via upload

--- chart/timeseries.js
+++ chart/timeseries.js
@@ -56,11 +56,11 @@ function drawBarChart(data) {
       labels: tmpLabels,
       datasets: [
         { label: "retail_and_recreation", data: tmpData1, borderColor: 'rgb(255, 99, 132)', borderWidth: 1,/* backgroundColor: "red" */},
-        { label: "grocery_and_pharmacy", data: tmpData2, borderColor: 'rgb(255, 99, 132)', borderWidth: 1,/* backgroundColor: "blue" */}
+        { label: "grocery_and_pharmacy", data: tmpData2, borderColor: 'rgb(255, 99, 132)', borderWidth: 1,/* backgroundColor: "blue" */},
 		{ label: "parks", data: tmpData3, borderColor: 'rgb(255, 99, 132)', borderWidth: 1,/* backgroundColor: "red" */},
-        { label: "transit_stations", data: tmpData4, borderColor: 'rgb(255, 99, 132)', borderWidth: 1,/* backgroundColor: "blue" */}
+        { label: "transit_stations", data: tmpData4, borderColor: 'rgb(255, 99, 132)', borderWidth: 1,/* backgroundColor: "blue" */},
 		{ label: "workplaces", data: tmpData5, borderColor: 'rgb(255, 99, 132)', borderWidth: 1,/* backgroundColor: "red" */},
-        { label: "residential", data: tmpData6, borderColor: 'rgb(255, 99, 132)', borderWidth: 1,/* backgroundColor: "blue" */}
+        { label: "residential", data: tmpData6, borderColor: 'rgb(255, 99, 132)', borderWidth: 1,/* backgroundColor: "blue" */},
       ]
     }
   });
