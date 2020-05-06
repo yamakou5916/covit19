3ace08996e3c09640ccd0b3314d05411e542d537
yamakou5916 <koki.yamamo10@gmail.com>
2020-05-06 13:06:06 +0900

    Add files via upload

--- chart/timeseries.js
+++ chart/timeseries.js
@@ -1,22 +1,82 @@
-                var ctx = document.getElementById('myChart').getContext('2d');
-                var chart = new Chart(ctx, {
-                // The type of chart we want to create
-                type: 'line',
-                // The data for our dataset
-                data: {
-                  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                  datasets: [{
-                    label: 'My First dataset',
-                    //backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [0, 10, 5, 2, 20, 30, 45]
-                  },{
-                    label: 'My Second dataset',
-                    //backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [2, 9, 14, 12, 10, 20, 35]
-                  }]
-                },
-                // Configuration options go here
-                options: {}
-              });
+/*
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+// The type of chart we want to create
+type: 'line',
+// The data for our dataset
+data: {
+	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+	datasets: [{
+		label: 'My First dataset',
+		//backgroundColor: 'rgb(255, 99, 132)',
+		borderColor: 'rgb(255, 99, 132)',
+		data: [0, 10, 5, 2, 20, 30, 45]
+	},{
+		label: 'My Second dataset',
+		//backgroundColor: 'rgb(255, 99, 132)',
+		borderColor: 'rgb(255, 99, 132)',
+		data: [2, 9, 14, 12, 10, 20, 35]
+	}]
+},
+// Configuration options go here
+options: {}
+});
+*/
+
+
+// 2) CSVから２次元配列に変換
+function csv2Array(str) {
+  var csvData = [];
+  var lines = str.split("\n");
+  for (var i = 0; i < lines.length; ++i) {
+    var cells = lines[i].split(",");
+    csvData.push(cells);
+  }
+  return csvData;
+}
+
+function drawBarChart(data) {
+  // 3)chart.jsのdataset用の配列を用意
+  var tmpLabels = [], tmpData1 = [], tmpData2 = [];
+  for (var row in data) {
+    tmpLabels.push(data[row][0])
+    tmpData1.push(data[row][1])
+    tmpData2.push(data[row][2])
+  };
+
+  // 4)chart.jsで描画
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: tmpLabels,
+      datasets: [
+        { label: "Tokyo", data: tmpData1, backgroundColor: "red" },
+        { label: "Osaka", data: tmpData2, backgroundColor: "blue" }
+      ]
+    }
+  });
+}
+
+
+
+
+
+function main() {
+  // 1) ajaxでCSVファイルをロード
+  var req = new XMLHttpRequest();
+  var filePath = 'data.csv';
+  req.open("GET", filePath, true);
+  req.onload = function() {
+    // 2) CSVデータ変換の呼び出し
+    data = csv2Array(req.responseText);
+    // 3) chart.jsデータ準備、4) chart.js描画の呼び出し
+    drawBarChart(data);
+  }
+  req.send(null);
+}
+
+main();
+
+
+
