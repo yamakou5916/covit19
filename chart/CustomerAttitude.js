1b5670ccd00c75fa3bd270e9668ddb39a1f3c411
yamakou5916 <koki.yamamo10@gmail.com>
2020-05-09 02:15:08 +0900

    Add files via upload

--- chart/CustomerAttitude.js
+++ chart/CustomerAttitude.js
@@ -1,37 +1,37 @@
 // 2) CSVから２次元配列に変換
-function csv2Array7(str) {
-  var csvData7 = [];
-  var lines7 = str.split("\n");
-  for (var i = 0; i < lines7.length; ++i) {
-    var cells7 = lines7[i].split(",");
-    csvData7.push(cells7);
+function csv2Array11(str) {
+  var csvData11 = [];
+  var lines11 = str.split("\n");
+  for (var i = 0; i < lines11.length; ++i) {
+    var cells11 = lines11[i].split(",");
+    csvData11.push(cells11);
   }
-  return csvData7;
+  return csvData11;
 }
 
-function drawBarChart7(data) {
+function drawBarChart11(data) {
   // 3)chart.jsのdataset用の配列を用意
-  var tmpLabels7 = [], tmpData17 = [], tmpData27 = [], tmpData37 = [], tmpData47 = [];
+  var tmpLabels11 = [], tmpData111 = [], tmpData211 = [], tmpData311 = [], tmpData411 = [];
   for (var row in data) {
-    tmpLabels7.push(data[row][0])
-    tmpData17.push(data[row][1])
-    tmpData27.push(data[row][2])
-    tmpData37.push(data[row][3])
-    tmpData47.push(data[row][4])
+    tmpLabels11.push(data[row][0])
+    tmpData111.push(data[row][1])
+    tmpData211.push(data[row][2])
+    tmpData311.push(data[row][3])
+    tmpData411.push(data[row][4])
 
   };
 
-  // 7)chart.jsで描画
-  var ctx7 = document.getElementById("Industry").getContext("2d");
-  var Industry = new Chart(ctx7, {
+  // 11)chart.jsで描画
+  var ctx11 = document.getElementById("CustomerAttitude").getContext("2d");
+  var CustomerAttitude = new Chart(ctx11, {
     type: 'line',
     data: {
-      labels: tmpLabels7,
+      labels: tmpLabels11,
       datasets: [
-        { label: "全産業活動指数", data: tmpData17, borderColor: 'rgb(77, 127, 189)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
-        { label: "建設業活動指数", data: tmpData27, borderColor: 'rgb(91, 192, 170)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "blue" */},
-        { label: "鉱工業生産指数", data: tmpData37, borderColor: 'rgb(188, 270, 277)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
-        { label: "第３次産業活動指数", data: tmpData47, borderColor: 'rgb(77, 127, 189)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
+        { label: "全産業活動指数", data: tmpData111, borderColor: 'rgb(77, 127, 189)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
+        { label: "建設業活動指数", data: tmpData211, borderColor: 'rgb(91, 192, 170)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "blue" */},
+        { label: "鉱工業生産指数", data: tmpData311, borderColor: 'rgb(188, 270, 277)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
+        { label: "第３次産業活動指数", data: tmpData411, borderColor: 'rgb(77, 127, 189)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
       ]
     },
 
@@ -39,12 +39,12 @@ function drawBarChart7(data) {
       scales: {
         
         xAxes: [{
-          Industryition: 'bottom',
+          CustomerAttitudeition: 'bottom',
           ticks: {
             maxRotation: 0, 
             minRotation: 0,
             autoSkip: true,
-            maxTicksLimit: 7, //値の最大表示数
+            maxTicksLimit: 11, //値の最大表示数
             fontColor: "white", // 文字の色
                     fontSize: 10,
           }
@@ -58,7 +58,7 @@ function drawBarChart7(data) {
       },
       legend: {
         display: true,
-        //Industryition: 'top',
+        //CustomerAttitudeition: 'top',
         labels: {
           fontSize: 10,
           boxWidth: 10,
@@ -70,21 +70,21 @@ function drawBarChart7(data) {
 }
 
 
-function main7() {
+function main11() {
   // 1) ajaxでCSVファイルをロード
-  var req7 = new XMLHttpRequest();
-  var filePath7 = './data/Industry.csv';
-  req7.open("GET", filePath7, true);
-  req7.onload = function() {
+  var req11 = new XMLHttpRequest();
+  var filePath11 = './data/CustomerAttitude.csv';
+  req11.open("GET", filePath11, true);
+  req11.onload = function() {
     // 2) CSVデータ変換の呼び出し
-    data7 = csv2Array7(req7.responseText);
-    // 3) chart.jsデータ準備、7) chart.js描画の呼び出し
-    drawBarChart7(data7);
+    data11 = csv2Array11(req11.responseText);
+    // 3) chart.jsデータ準備、11) chart.js描画の呼び出し
+    drawBarChart11(data11);
   }
-  req7.send(null);
+  req11.send(null);
 }
 
-main7();
+main11();
 
 
 
