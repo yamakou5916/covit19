22bcf060165f279ec33df9c0b921fbae90dbd91d
yamakou5916 <koki.yamamo10@gmail.com>
2020-05-09 01:37:03 +0900

    Add files via upload

--- chart/Industry.js
+++ chart/Industry.js
@@ -1,37 +1,36 @@
 // 2) CSVから２次元配列に変換
-function csv2Array7(str) {
-  var csvData7 = [];
-  var lines7 = str.split("\n");
-  for (var i = 0; i < lines7.length; ++i) {
-    var cells7 = lines7[i].split(",");
-    csvData7.push(cells7);
+function csv2Array10(str) {
+  var csvData10 = [];
+  var lines10 = str.split("\n");
+  for (var i = 0; i < lines10.length; ++i) {
+    var cells10 = lines10[i].split(",");
+    csvData10.push(cells10);
   }
-  return csvData7;
+  return csvData10;
 }
 
-function drawBarChart7(data) {
+function drawBarChart10(data) {
   // 3)chart.jsのdataset用の配列を用意
-  var tmpLabels7 = [], tmpData17 = [], tmpData27 = [], tmpData37 = [], tmpData47 = [];
+  var tmpLabels10 = [], tmpData110 = [], tmpData210 = [], tmpData310 = [], tmpData410 = [];
   for (var row in data) {
-    tmpLabels7.push(data[row][0])
-    tmpData17.push(data[row][1])
-    tmpData27.push(data[row][2])
-    tmpData37.push(data[row][3])
-    tmpData47.push(data[row][4])
-
+    tmpLabels10.push(data[row][0])
+    tmpData110.push(data[row][1])
+    tmpData210.push(data[row][2])
+    tmpData310.push(data[row][3])
+    tmpData410.push(data[row][4])
   };
 
-  // 7)chart.jsで描画
-  var ctx7 = document.getElementById("Industry").getContext("2d");
-  var Industry = new Chart(ctx7, {
+  // 10)chart.jsで描画
+  var ctx10 = document.getElementById("Industry").getContext("2d");
+  var Industry = new Chart(ctx10, {
     type: 'line',
     data: {
-      labels: tmpLabels7,
+      labels: tmpLabels10,
       datasets: [
-        { label: "全産業活動指数", data: tmpData17, borderColor: 'rgb(77, 127, 189)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
-        { label: "建設業活動指数", data: tmpData27, borderColor: 'rgb(91, 192, 170)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "blue" */},
-        { label: "鉱工業生産指数", data: tmpData37, borderColor: 'rgb(188, 270, 277)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
-        { label: "第３次産業活動指数", data: tmpData47, borderColor: 'rgb(77, 127, 189)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
+        { label: "全産業活動指数", data: tmpData110, borderColor: 'rgb(1010, 1210, 1109)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
+        { label: "建設業活動指数", data: tmpData210, borderColor: 'rgb(91, 192, 1100)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "blue" */},
+        { label: "鉱工業生産指数", data: tmpData310, borderColor: 'rgb(11010, 2100, 21010)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
+        { label: "第３次産業活動指数", data: tmpData410, borderColor: 'rgb(1010, 1210, 1109)', borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
       ]
     },
 
@@ -44,7 +43,7 @@ function drawBarChart7(data) {
             maxRotation: 0, 
             minRotation: 0,
             autoSkip: true,
-            maxTicksLimit: 7, //値の最大表示数
+            maxTicksLimit: 10, //値の最大表示数
             fontColor: "white", // 文字の色
                     fontSize: 10,
           }
@@ -70,21 +69,21 @@ function drawBarChart7(data) {
 }
 
 
-function main7() {
+function main10() {
   // 1) ajaxでCSVファイルをロード
-  var req7 = new XMLHttpRequest();
-  var filePath7 = './data/Industry.csv';
-  req7.open("GET", filePath7, true);
-  req7.onload = function() {
+  var req10 = new XMLHttpRequest();
+  var filePath10 = './data/Industry.csv';
+  req10.open("GET", filePath10, true);
+  req10.onload = function() {
     // 2) CSVデータ変換の呼び出し
-    data7 = csv2Array7(req7.responseText);
-    // 3) chart.jsデータ準備、7) chart.js描画の呼び出し
-    drawBarChart7(data7);
+    data10 = csv2Array10(req10.responseText);
+    // 3) chart.jsデータ準備、10) chart.js描画の呼び出し
+    drawBarChart10(data10);
   }
-  req7.send(null);
+  req10.send(null);
 }
 
-main7();
+main10();
 
 
 
