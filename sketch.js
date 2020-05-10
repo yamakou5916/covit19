ef4874cd2adaa3244cca10aded0b44f98ff2ef2e
yamakou5916 <koki.yamamo10@gmail.com>
2020-05-10 23:53:33 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -97,11 +97,11 @@ function draw(){
   textAlign(CENTER,CENTER);
   //line(width/2,80,width/2,height-80);
   //line(200,height/2,width-200,height/2);
-  line(width/2,0,width/2,height-80);
-  line(200,height/2,width-200,height/2);
+  line(width/2,30,width/2,height-30);
+  line(50,height/2,width-50,height/2);
   noStroke();
   fill(255);
-  textSize(32);
+  textSize(3);
   text("Politics", 200, 50);
   text("Economics", width-200, 50);
   text("Society", 200, height-50);
