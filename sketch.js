99243be79b04aba1d28caa6ef51fc2424f67de57
yamakou5916 <koki.yamamo10@gmail.com>
2020-05-11 23:38:10 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -98,7 +98,7 @@ function draw(){
   //line(width/2,80,width/2,height-80);
   //line(200,height/2,width-200,height/2);
   line(width/2,60,width/2,height-60);
-  line(200,height/2,width-200,height/2);
+  line(150,height/2,width-150,height/2);
   noStroke();
   fill(255);
   textSize(32);
