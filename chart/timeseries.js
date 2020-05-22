e9c4dc23f3bfea5270b74c974c49f70b725ec37d
yamakou5916 <koki.yamamo10@gmail.com>
2020-05-23 01:54:04 +0900

    Add files via upload

--- chart/timeseries.js
+++ chart/timeseries.js
@@ -65,9 +65,75 @@ function drawBarChart(data) {
         { label: "公共交通機関", data: tmpData4, borderColor: colors[3], borderWidth: 1, pointRadius: 0,/* backgroundColor: "blue" */},
         { label: "職場", data: tmpData5, borderColor: colors[4], borderWidth: 1, pointRadius: 0,/* backgroundColor: "red" */},
         { label: "住居", data: tmpData6, borderColor: colors[5], borderWidth: 1, pointRadius: 0,/* backgroundColor: "blue" */},
-      ]
+      ],
+      graphs: [
+        {type: 'line',name: '折れ線グラフ'},
+        {type: 'bar',name: '棒グラフ'},
+        {type: 'radar',name: 'レーダーチャート'},
+        {type: 'pie',name: 'パイチャート'},
+        {type: 'polarArea',name: 'ポーラチャート'}
+      ],
+      graphSelected: 'line'
     },
+    watch: {
+      datasets: {
+        handler() {
+          this.canvas.data.datasets[0].data = this.datasets;
+          this.canvas.data.labels = Array(this.datasets.length).fill('');
+          this.canvas.update();
+        }
+      },
+      graphSelected: {
+        handler() {
+          this.canvas.destroy();
+          this.chart();
+        }
+      }
+    },
+        mounted() {
+        this.chart();
+    },
+    methods: {
+      toRaw(data) {
+        return JSON.stringify(data);
+      },
+      add(i) {
+        this.datasets.splice(i, 0, this.datasets[i]);
+      },
+      del(i) {
+        if (this.datasets.length > 1) {
+          this.datasets.splice(i, 1);
+        }
+      },
+      chart() {
+        var vm = this;
+        var ctx = document.getElementById("charts");
+        vm.canvas = new Chart(ctx, {
+          type: vm.graphSelected,
+          data: {
+            labels: Array(vm.datasets.length).fill(''),
+            datasets: [{
+              label: 'グラフ',
+              backgroundColor: '#20B2AA',
+              data: vm.datasets
+            }]
+          }
+        });
+      },
+      randData() {
+        var index = Math.floor(Math.random() * (10 - 2) + 2);
+        var i = 0;
+        var datasets = [];
+        while (index > i) {
+          datasets.push(Math.floor(Math.random() * (1000) - 500));
+          i++;
+        }
+        this.datasets = datasets;
+      }
+    }
+  })
 
+    /*
     options: {
     	scales: {
     		xAxes: [{
@@ -102,7 +168,7 @@ function drawBarChart(data) {
 
   });
 }
-
+*/
 
 function main() {
   // 1) ajaxでCSVファイルをロード
