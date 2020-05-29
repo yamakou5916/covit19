17679102b0dcf1d31d03d795434435a808ce72da
yamakou5916 <koki.yamamo10@gmail.com>
2020-05-29 20:49:46 +0900

    Add files via upload

--- chart/EmploymentStatus.js
+++ chart/EmploymentStatus.js
@@ -31,8 +31,8 @@ function drawBarChart12(data) {
       labels: tmpData12[0],
       datasets: [
         /*{ label: "非正規率", data: tmpData112, borderColor: colors[0], borderWidth: 1, pointRadius: 0, yAxisID: "y-axis-2",},*/
-        { label: "正規の職員・従業員", data: tmpData12[2], borderColor: colors[1], borderWidth: 1, pointRadius: 0, yAxisID: "y-axis-1", backgroundColor: "rgba(255,255,255,0)"},
-        { label: "非正規の職員・従業員", data: tmpData12[3], borderColor: colors[2], borderWidth: 1, pointRadius: 0, yAxisID: "y-axis-1", backgroundColor: "rgba(255,255,255,0)"},
+        { label: "正規の職員・従業員", data: tmpData12[2], borderColor: colors[1], borderWidth: 1, pointRadius: 0, yAxisID: "y-axis-1", fillColor: "rgba(255,255,255,0)"},
+        { label: "非正規の職員・従業員", data: tmpData12[3], borderColor: colors[2], borderWidth: 1, pointRadius: 0, yAxisID: "y-axis-1", fillColor: "rgba(255,255,255,0)"},
       ]
     },
 
