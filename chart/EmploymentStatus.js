7cd0cda633f013d7d22569215e2df2e40f9f9ff8
yamakou5916 <koki.yamamo10@gmail.com>
2020-06-05 09:18:52 +0900

    Add files via upload

--- chart/EmploymentStatus.js
+++ chart/EmploymentStatus.js
@@ -22,7 +22,7 @@ function drawBarChart12(data) {
     tmpData12[3].push(data[row][3])
     tmpData12[4].push(data[row][4])
     tmpData12[5].push(data[row][5])
-    tmpData12[6].push(data[row][6])
+    tmpData12[6].push(data[row][6]-data[row][5])
   };
   var demo = document.getElementById("samurai4");
   demo.innerHTML = (data[row][2]-50).toFixed(1);
