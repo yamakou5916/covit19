d8c637ae9c1de7d1e14ec91ba561e6c1434e84e1
yamakou5916 <koki.yamamo10@gmail.com>
2020-06-06 20:49:42 +0900

    Add files via upload

--- chart/POS2.js
+++ chart/POS2.js
@@ -107,7 +107,7 @@ function func0() {
   // form要素を取得
   var element = document.getElementById( "target0" ) ;
   // form要素内のラジオボタングループ(name="hoge")を取得
-  var radioNodeList = element.hoge ;
+  //var radioNodeList = element.hoge ;
   // 2つ目の要素を選択状態にする
   //radioNodeList[1].checked = true ;
   // 選択状態の値(value)を取得 (Bが選択状態なら"b"が返る)
@@ -120,11 +120,9 @@ function func0() {
 }
 
 
-var element = document.getElementById( "target0" ) ;
+//var element = document.getElementById( "target0" ) ;
 // form要素内のラジオボタングループ(name="hoge")を取得
-var radioNodeList = element.hoge ;
-// 2つ目の要素を選択状態にする
-//radioNodeList[1].checked = true ;
+//var radioNodeList = element.hoge ;
 
 func0();
 
