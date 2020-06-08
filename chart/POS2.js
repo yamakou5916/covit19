ca56faa5b287dc4a2b3eea2c6b4564ae2125b9da
yamakou5916 <koki.yamamo10@gmail.com>
2020-06-08 16:44:42 +0900

    Add files via upload

--- chart/POS2.js
+++ chart/POS2.js
@@ -1,5 +1,6 @@
 var colors = ["#7BDFF2","#B2F7EF","#EFF7F6","#F7D6E0","#F2B5D4","#DABFFF"];
 var posshop = 0;
+var beforeafter = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 // 2) CSVから２次元配列に変換
 function csv2Array20(str, a) {
