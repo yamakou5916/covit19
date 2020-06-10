debbbb3f01d9f3a113ffd85d95f87d6045bef4a5
yamakou5916 <koki.yamamo10@gmail.com>
2020-06-10 12:30:46 +0900

    Add files via upload

--- chart/Bankruptcy.js
+++ chart/Bankruptcy.js
@@ -1,13 +1,28 @@
 var colors = ["#7BDFF2","#B2F7EF","#EFF7F6","#F7D6E0","#F2B5D4","#DABFFF"];
+var bankrupt = 0;
+var beforeafter = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 // 2) CSVから２次元配列に変換
-function csv2Array4(str) {
+function csv2Array4(str,a) {
   var csvData4 = [];
   var lines4 = str.split("\n");
-  for (var i = 0; i < lines4.length; ++i) {
+  if(a == "a"){
+    var num0 = 12;
+    var num1 = 0;
+  }else{
+    var num0 = 24;
+    var num1 = 12;
+  }
+  for (var i = lines4.length-num0; i < lines4.length-num1; ++i) {
     var cells4 = lines4[i].split(",");
     csvData4.push(cells4);
   }
+  var k=0;
+  for (var j = lines4.length-13; j < lines4.length; ++j) {
+    beforeafter[k] = lines4[j].split(",");
+    ++k;
+  }
+  bankrupt = (((beforeafter[12][2]/beforeafter[0][2])-1)*100).toFixed(1);
   return csvData4;
 }
 
@@ -90,19 +105,46 @@ function drawBarChart4(data) {
 }
 
 
-function main4() {
+function main4(a) {
   // 1) ajaxでCSVファイルをロード
   var req4 = new XMLHttpRequest();
   var filePath4 = './data/Bankruptcy.csv';
   req4.open("GET", filePath4, true);
   req4.onload = function() {
     // 2) CSVデータ変換の呼び出し
-    data4 = csv2Array4(req4.responseText);
+    data4 = csv2Array4(req4.responseText,a);
     // 3) chart.jsデータ準備、4) chart.js描画の呼び出し
     drawBarChart4(data4);
   }
   req4.send(null);
 }
+
+function func7() {
+  // form要素を取得
+  var element = document.getElementById( "target7" ) ;
+  // form要素内のラジオボタングループ(name="hoge")を取得
+  var radioNodeList = element.hoge ;
+  // 選択状態の値(value)を取得 (Bが選択状態なら"b"が返る)
+  var a = radioNodeList.value ;
+  if ( a === "" ) {
+    // 未選択状態
+  } else {
+    // aには選択状態の値が代入されている
+    //console.log( a ) ;
+    main3(a)
+  }
+}
+
+
+function ratedata7(){
+  return bankrupt;
+}
+
+
+var element = document.getElementById( "target7" ) ;
+var radioNodeList = element.hoge ;
+radioNodeList[0].checked = true ;
+func7();
 
 main4();
 
