77c3f8f24c83d5a3640adfe6d55b8b48e7bb393a
yamakou5916 <koki.yamamo10@gmail.com>
2020-06-10 13:47:03 +0900

    Add files via upload

--- chart/Bankruptcy.js
+++ chart/Bankruptcy.js
@@ -45,8 +45,8 @@ function drawBarChart4(data) {
     data: {
       labels: tmpLabels4,
       datasets: [
-        { label: "倒産数", data: tmpData14, borderColor: colors[0], borderWidth: 1, pointRadius: 0, yAxisID: "y-axis-1", backgroundColor: colors[0]},
-        { type: 'line',label: "負債総額", data: tmpData24, borderColor: colors[1], borderWidth: 1, pointRadius: 0,yAxisID: "y-axis-2", backgroundColor: "rgba(255,255,255,0)"},
+        { label: "倒産数（件）", data: tmpData14, borderColor: colors[0], borderWidth: 1, pointRadius: 0, yAxisID: "y-axis-1", backgroundColor: colors[0]},
+        { type: 'line',label: "負債総額（万円）", data: tmpData24, borderColor: colors[1], borderWidth: 1, pointRadius: 0,yAxisID: "y-axis-2", backgroundColor: "rgba(255,255,255,0)"},
       ]
     },
 
