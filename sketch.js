cf53584d3e6269ecaa4ae9bda9c4850806b18642
yamakou5916 <koki.yamamo10@gmail.com>
2020-06-13 22:39:46 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -56,7 +56,8 @@ function setup(){
   changeRate[7] = ratedata7();
   changeRate[8] = ratedata8();
   pixelDensity(2);
-  canvas = createCanvas(2000, 1000);//描画領域を指定
+  canvas = createCanvas(windowWidth, windowHeight);//描画領域を指定
+  //canvas = createCanvas(2000, 1000);//描画領域を指定
   canvas.parent("P5Canvas");
   background(255);
   backphoto.push(loadImage("image/0.jpg"));
