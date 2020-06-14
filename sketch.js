63bc634c30ee4b5e84634c11472610c864f7a8f5
yamakou5916 <koki.yamamo10@gmail.com>
2020-06-15 01:12:12 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -105,7 +105,7 @@ function draw(){
   //fill(1,1,27);
   //rect(0,0,2000,1500);
   //image(backphoto[textnum], windowWidth, windowHeight, 2000, 1500);
-  image(backphoto[textnum], 1000, 750, 2000, 1500);
+  //image(backphoto[textnum], 1000, 750, 2000, 1500);
   var backImage = document.getElementById("P5Canvas");
   //var backImage = document.getElementsByClassName("box box3");
   backImage.style.backgroundImage = "image/0.jpg";
